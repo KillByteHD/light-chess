577df9e537b7a306fac8a6d978b2f7ff52eafe21
KillByte <killbyte.games@gmail.com>
2020-07-27 12:32:30 +0100

    + Board setup

--- include/board.hpp
+++ include/board.hpp
@@ -1,18 +1,35 @@
 #ifndef PIECE_HPP
 #define PIECE_HPP
 
+#include <cstdint>
+#include <vector>
+#include <iostream>
+
+#define PAWN 1
+#define KNIGHT 2
+#define BISHOP 3
+#define ROOK 4
+#define QUEEN 5
+#define KING 6
+
+#define WHITE 0
+#define BLACK 1
+
 namespace light_chess
 {
 
     using piece = int8_t;
-    using piece_color = bool;
+    using piece_color = int8_t;
+    using position = char[2];
 
-    constexpr piece make_pawn();
-    constexpr piece make_knight();
-    constexpr piece make_bishop();
-    constexpr piece make_rook();
-    constexpr piece make_queen();
-    constexpr piece make_king();
+    constexpr piece make_pawn(const piece_color);
+    constexpr piece make_knight(const piece_color);
+    constexpr piece make_bishop(const piece_color);
+    constexpr piece make_rook(const piece_color);
+    constexpr piece make_queen(const piece_color);
+    constexpr piece make_king(const piece_color);
+
+    constexpr char representation(const piece);
 
     //class piece
     //{
@@ -28,9 +45,39 @@ namespace light_chess
         private:
             piece data[8][8];
         public:
-            
+            board() = default;
+
+            piece& operator[](const position pos)
+            {
+                return data[pos[0]-'a'][pos[1]-'1'];
+            }
+
+            piece* operator[](const uint pos)
+            {
+                return data[pos];
+            }
+
+
+            bool move(const position from, const position to);
+
+            std::vector<position> moves(const position pos);
 
     };
+
+
+    
+    void print(board b)
+    {
+        //std::cout << "+-+-+-+-+-+-+-+-+\n";
+        for(uint i = 0 ; i < 8 ; ++i)
+        {
+            std::cout << "+-+-+-+-+-+-+-+-+\n";
+            for(uint j = 0 ; j < 8 ; ++j)
+                printf("|%d", int(b[i][j]));
+            std::cout << "|\n";
+        }
+        std::cout << "+-+-+-+-+-+-+-+-+\n";
+    }
 }
 
 
--- src/board.cpp
+++ src/board.cpp
@@ -5,12 +5,54 @@
 
 namespace light_chess
 {
+    constexpr piece __make_piece(const piece pce, const piece_color clr)
+    {
+        return (clr) ? -pce : pce;
+    }
+
+    constexpr piece make_pawn(const piece_color clr)
+    {
+        return __make_piece(PAWN,clr);
+    }
+    
+    constexpr piece make_knight(const piece_color clr)
+    {
+        return __make_piece(KNIGHT,clr);
+    }
+
+    constexpr piece make_bishop(const piece_color clr)
+    {
+        return __make_piece(BISHOP,clr);
+    }
+
+    constexpr piece make_rook(const piece_color clr)
+    {
+        return __make_piece(ROOK,clr);
+    }
+
+    constexpr piece make_queen(const piece_color clr)
+    {
+        return __make_piece(QUEEN,clr);
+    }
+
+    constexpr piece make_king(const piece_color clr)
+    {
+        return __make_piece(KING,clr);
+    }
+
+
     
 }
 
+namespace lc = light_chess;
+
 int main(void)
 {
-    const int8_t n1 = -2;
-    std::cout << std::abs(n1) << '\n';
+    lc::board brd{};
+    //lc::piece pwn = lc::make_pawn(BLACK);
+    //brd.at("a1");
+    std::cout << int(brd["a1"]) << '\n';
+    
+    print(brd);
     return 0;
 }
