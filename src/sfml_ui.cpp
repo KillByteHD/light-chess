859b9781a093f8b00916206ae6bc629e263b10e8
KillByte <killbyte.games@gmail.com>
2020-08-21 22:52:07 +0100

    + Init light_ui

--- src/sfml_ui.cpp
+++ src/sfml_ui.cpp
@@ -1,10 +1,47 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
+#include <vector>
+
+#include "chess.hpp"
+
+namespace lc = light_chess;
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    sf::RenderWindow window(sf::VideoMode(800, 800), "LightChess");
+    const sf::Color clear_color(sf::Color::Black /* 21, 94, 133 */ );
+
+    sf::Texture tex;
+    if (!tex.loadFromFile("assets/textures_1.png",sf::IntRect(0, 0, 152, 152)))
+    {
+        std::cerr << "No texture\n";
+        return -1;
+    }
+
+    std::array<sf::Sprite,6> white_pieces;
+    std::array<sf::Sprite,6> black_pieces;
+
+    lc::chess_game gaem;
+
+    sf::Sprite sprite;
+    sprite.setTexture(tex);
+
+    
+    std::vector<sf::RectangleShape> board;
+
+    for(uint i = 0 ; i < 8 ; ++i)
+    {
+        for(uint j = 0 ; j < 8 ; ++j)
+        {
+            if((i+j) & 1)
+            {
+                sf::RectangleShape rect({100,100});
+                rect.setPosition(100*i,100*j);
+                board.push_back(rect);
+            }
+        }
+    }
+
 
     while (window.isOpen())
     {
@@ -15,8 +52,11 @@ int main()
                 window.close();
         }
 
-        window.clear();
-        window.draw(shape);
+        window.clear(clear_color);
+        
+        for(const auto& rect : board)
+            window.draw(rect);
+        
         window.display();
     }
 
